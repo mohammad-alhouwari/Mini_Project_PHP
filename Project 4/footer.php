f678d98d206c80612d99b04acc2b0560c99e70e6
mohammad-alhouwari <mohammad.alouwari.1996@gmail.com>
2023-08-03 23:07:41 +0300

    test

---
+++ Project 4/footer.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="./footer.css">
+<footer style="background-color: #9A208C!important;">
+    <div id="foot">
+      <div id="footCont">
+        <div class="col text-center">
+          <!-- Logo -->
+          <img src="../image/LOGO_TOYS.png" alt="Your Logo">
+        </div>
+        <div class="col text-center">
+          <!-- Contact Details -->
+          <p><b>Contact us</b></p>
+          <p>Email:mohammad.alhouwari.1996@gmail.com</p>
+          <p>Phone: +962788120617</p>
+        </div>
+      </div>
+      <div>
+        <div class="col text-center">
+          <!-- Copyright -->
+          <p>&copy; 2023 Your Company. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ Project 4/header.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="./header.css">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark" style="background-color: #9A208C!important;">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="../image/LOGO_TOYS.png" alt="logo" style="width:40px;">
+  </a>
+
+  <!-- Burger menu button -->
+  <button class="navbar-toggler ml-auto " type="button" data-toggle="collapse" data-target="#navbarMenu">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Links -->
+  <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#TOP">TOP</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#ITEMS">ITEMS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#FOOTER">FOOTER</a>
+      </li>
+    </ul>
+  </div>
+</nav>
